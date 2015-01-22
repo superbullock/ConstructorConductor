4bb3547bee2fdb19be5688d8497b6fed4c8f2aae
DANIEL BULLOCK <DANIEL.I.BULLOCK@gmail.com>
2015-01-21 19:54:07 -0700

    "finished arrayproperty.js"

--- arrayProperty.js
+++ arrayProperty.js
@@ -3,3 +3,14 @@
 //Add a reverse method to the String 'class' so that every instance of String can call reverse and reverse itself.
   
   //code here
+String.prototype.myReverse = function () {
+    var str = this;
+    str = this.split('');
+    for (var i = 0; i < this.length / 2; i++) {
+        var temp = str[i];
+        str[i] = str[str.length - 1 - i];
+        str[str.length - 1 - i] = temp;
+    }
+    str = str.join('');
+    return str;
+}
