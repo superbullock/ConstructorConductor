ae383514a3334fa09d62d566ceaa44254f8632df
DANIEL BULLOCK <DANIEL.I.BULLOCK@gmail.com>
2015-01-21 19:37:21 -0700

    "completed sayName.js"

--- sayName.js
+++ sayName.js
@@ -1,13 +1,22 @@
 //Create a Person constructor that accepts name and age as parameters and sets those properties accordingly in the Constructor.
 
   //code here
-
+var Person = function (name, age) {
+    this.name = name;
+    this.age = age;
+};
+    
 
 //Now create three instances of Person with data you make up
 
   //code here
-
+var me = new Person('daniel', 26);
+var brian = new Person('brian', 27);
+var stephen = new Person('stephen', 28);
 
 //Now add a sayName method on your Person class that will alert the name of whatever Person instance called it.
 
   //code here
+Person.prototype.sayName = function() {
+    alert('hello ' + this.name);
+};
